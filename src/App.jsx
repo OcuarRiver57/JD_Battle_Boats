09e72b6715cb864bc8018449950f9e666d174765
Jacob Dykstra <dykstraj@my.lanecc.edu>
2025-11-23 17:53:37 -0800

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -8,66 +8,191 @@
 
 // where i left off
 /*
-
+displayshipslist > ShipForList > onShipListClick
 */
 
+import { useState, useEffect, createContext, useContext } from 'react';
 
-import { useState, useEffect } from 'react';
+// create a context to avoid prop drilling for grids and styles
+const GameContext = createContext(null);
 
-// global variables
-// in lowercase with underscores to make them easily identifiable
+// global variables in lowercase with underscores to make them easily identifiable
 let PLAYER_ACTION = "placing_ship"; // possible values: "placing_ship" "moving_ship" "attacking"
 let PLAYER_TURN = 1; // 1 or 2
 
-let P1_SHIP_MAP = [[],[],[],[],[],[],[],[],[],[]]; 
-let P1_ATTACK_MAP = [[],[],[],[],[],[],[],[],[],[]]; 
-let P2_SHIP_MAP = [[],[],[],[],[],[],[],[],[],[]]; 
-let P2_ATTACK_MAP = [[],[],[],[],[],[],[],[],[],[]]; 
+let P1_SHIP_DATA = {}; //dictionary of ship placements for player 1, keyed by cell id (e.g. "a-0-0")
+let P1_ATTACK_DATA = {}; // dictionary of attack history for player 1, keyed by cell id (e.g. "b-0-0")
+let P1_SHIP_LIST = {}; // dictionary of ships for player 1
+// example {ship1 : [cord1, cord2, cord3]}, {ship2 : [cord1, cord2, cord3]}, etc.
 
+let P2_SHIP_DATA = {}; 
+let P2_ATTACK_DATA = {};
+let P2_SHIP_LIST = {};
 
 function App() {
-  // Initialize two 10x10 grids
-  const [gridA, setGridA] = useState(CreateBlankMap());
-  const [gridB, setGridB] = useState(CreateBlankMap());
+  const [gridA, setGridA] = useState(CreateBlankGridDict("a"));
+  const [gridB, setGridB] = useState(CreateBlankGridDict("b"));
+
+  useEffect(() => {
+    LoadMapData(setGridA, setGridB);
+  }, []);
+
+  let className = " bg-black text-white mx-2 border-2 border-gray-500 p-2 m-4";
+  let cellStyle = "flex items-center justify-center hover:cursor-pointer";
 
   return (
-    <div id="app_container" className = "flex flex-col h-screen">
-      <div id = "info_bar" className = "flex flex-row justify-between bg-gray-300 p-4">
-        <div id = "turn_info" className = "flex-6/12">Current Player: null</div>
-        <div id = "timer_info" className = "flex-6/12">Game Time:0    Round Time:0</div>
+    <GameContext.Provider value={{ gridA, setGridA, gridB, setGridB, gridStyle: className, cellStyle }}>
+    <div id="app_container" className = "flex flex-col h-screen max-h-screen">
+        <div id = "info_bar" className = "flex flex-row justify-between bg-gray-300 p-4">
+          <div id = "turn_info" className = "flex-6/12">Player{PLAYER_TURN}'s Turn!</div>
+          <div id = "timer_info" className = "flex-6/12">Game Time:0    Round Time:0</div>
+        </div>
+
+        {(PLAYER_ACTION == "placing_ship") 
+        ?  <GameStart /> 
+        :  <GamePlay />}
+
+        <div id = "base_bar" className = "flex flex-row justify-left bg-gray-300 p-4">
+          <div id = "powerup_area" className = "bg-yellow-400 flex-5/12"> power ups</div>
+          <div id = "status_area" className = "bg-green-400 flex-5/12"> <AddTestingButtons />{/*remove this before production */} </div>
+        </div>
+      </div>
+      </GameContext.Provider>
+  )
+  
+}
+
+function GameStart() {
+  const { gridA, setGridA, gridStyle, cellStyle } = useContext(GameContext);
+  const fleet = {
+  "patrol": {
+    "id": "patrol",
+    "name": "Patrol Boat",
+    "amount": 2,
+    "length": 2
+  },
+  "submarine": {
+    "id": "submarine",
+    "name": "Submarine",
+    "amount": 2,
+    "length": 3
+  },
+  "destroyer": {
+    "id": "destroyer",
+    "name": "Destroyer",
+    "amount": 2,
+    "length": 3
+  },
+  "battleship": {
+    "id": "battleship",
+    "name": "Battleship",
+    "amount": 1,
+    "length": 4
+  },
+  "carrier": {
+    "id": "carrier",
+    "name": "Carrier",
+    "amount": 1,
+    "length": 5
+  }
+};
+
+  const [currentShip, setCurrentShip] = useState("patrol");
+  const [direction, setDirection] = useState("up");
+
+  return (
+    <div id="game_board" className = "flex flex-row flex-1 bg-gray-700 justify-center">
+
+      <div id = "fleet_list" className = {`${gridStyle}`}>
+        <DisplayShipList 
+          fleet={fleet}
+          currentShip={currentShip}
+          setCurrentShip={setCurrentShip}
+          direction={direction}
+          setDirection={setDirection}
+        />
       </div>
 
-      <div id="game_board" className = "flex flex-row h-10/12">
-        <div id = "ship_grid" className = "flex-5/12 flex-col bg-blue-400 mx-5"> 
-          <CreateGrid gridId="a" grid={gridA} setGrid={setGridA}/>
-        </div>
-        <div id = "attack_grid" className = "flex-5/12 flex-col bg-black text-white mx-5">
-          <CreateGrid gridId="b" grid={gridB} setGrid={setGridB}/>
-        </div>
+      <div id = "ship_grid" className = {`${gridStyle} aspect-square`}> 
+        <CreateDisplayGrid 
+          gridId="a" 
+          cellStyle={cellStyle}
+          />
       </div>
+      
+    </div>
+  )
 
-      <div id = "base_bar" className = "flex flex-row justify-left bg-gray-300 p-4">
-        <div id = "powerup_area" className = "bg-yellow-400 flex-5/12"> power ups</div>
-        <div id = "status_area" className = "bg-green-400 flex-5/12"> <AddTestingButtons setGridA={setGridA} setGridB={setGridB}/>{/*remove this before production */} </div>
+}
+
+function GamePlay() {
+  const { gridStyle, cellStyle } = useContext(GameContext);
+  let className = "aspect-square flex-1 flex flex-col";
+  let gs = `${gridStyle} ${className}`;
+
+  let cs = `${cellStyle} hover:border hover:border-orange-500`;
+
+  return (
+    <div id="game_board" className = "flex flex-row flex-1 bg-gray-700">
+      <div id = "ship_grid" className = {gs}> 
+        <CreateDisplayGrid
+          gridId="a" 
+          cellStyle={cs}
+        />
       </div>
+      <div id = "attack_grid" className = {gs}>
+        <CreateDisplayGrid 
+          gridId="b" 
+          cellStyle={cs}
+        />
+      </div>
+    </div>
+    )
+}
+
+function DisplayShipList({fleet, currentShip, setCurrentShip, direction, setDirection}){
+  return Object.keys(fleet).map(key => (
+    <ShipForList key={`shipList_${fleet[key].name}`} ship={fleet[key]} currentShip={currentShip} setCurrentShip={setCurrentShip} />
+  ));
+}
+
+function ShipForList({ship, currentShip, setCurrentShip}){
+  const { gridA, setGridA } = useContext(GameContext);
+  
+  let name = ship["name"];
+  let shipkey = ship["id"];
+  let length = ship["length"];
+  let amount = ship["amount"];
+  
+  const onShipListClick = () => {
+    setCurrentShip(shipkey);
+    console.log(`selected ${shipkey}`);
+  };
+  
+  return(
+    <div 
+      id = {`shipList_${name}`} 
+      key = {`shipList_${name}`} 
+      className = "hover:cursor-pointer border border-black hover:border-orange-600 pb-1 mb-4"
+      onClick={onShipListClick}
+    >
+      <p className = "bg-gray-700 text-white px-1">{name + "s"}</p>
+      <p>Length: {length}</p>
+      <p>Remaining: {amount}</p>
     </div>
   )
 }
 
-//#region grid creation
-function Cell({ gridID, row, col, value, onUpdate }) {
+function Cell({ gridID, row, col, value, cellStyle}) {
   const cellId = `${gridID}-${row}-${col}`;
 
   return (
     <div 
       id={cellId} 
       key={cellId}
-      className="
-        w-full h-full border border-gray-500 flex items-center justify-center 
-        hover:cursor-pointer hover:bg-orange-500
-      " 
+      className={cellStyle} 
       style={{ backgroundColor: SetBackgroundOfCell(value) }}
-      onClick={() => handleCellClick(cellId, row, col, gridID, onUpdate)}
+      onClick={() => handleCellClick(cellId)}
     >
     {/* cell contents */}
     {value}
@@ -75,9 +200,13 @@ function Cell({ gridID, row, col, value, onUpdate }) {
   ); 
 }
 
-function CreateGrid({ gridId, grid, setGrid, rows = 10, cols = 10 }) {
+function CreateDisplayGrid({ gridId, cellStyle, rows = 10, cols = 10 }) {
     // function to handle when a cell is clicked and update it with a new value
     const handleCellUpdate = (row, col, newValue) => {
+      const { gridA, gridB, setGridA, setGridB } = useContext(GameContext);
+      const grid = gridId === 'a' ? gridA : gridB;
+      const setGrid = gridId === 'a' ? setGridA : setGridB;
+
       // make a copy of the entire grid so we can change it
       let newGrid = [];
       for (let y = 0; y < grid.length; y++) {
@@ -97,16 +226,19 @@ function CreateGrid({ gridId, grid, setGrid, rows = 10, cols = 10 }) {
 
     // create an array to hold all the cell components
     let cells = [];
+    const { gridA, gridB } = useContext(GameContext);
+    let gridData = gridId === 'a' ? gridA : gridB;
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
+        let cellId = `${gridId}-${row}-${col}`;
         cells.push(
           <Cell 
-            key={`${gridId}-${row}-${col}`} 
+            key={cellId} 
             gridID={gridId} 
             row={row} 
             col={col}
-            value={grid[row][col]}
-            onUpdate={handleCellUpdate}
+            value={gridData[cellId] || ""}
+            cellStyle={cellStyle}
           />
         );
       }
@@ -118,151 +250,86 @@ function CreateGrid({ gridId, grid, setGrid, rows = 10, cols = 10 }) {
     );
   }
 
-function handleCellClick(cellId, row, col, gridId, onUpdate) {
+function handleCellClick(cellId) {
   const cell = document.getElementById(cellId);
   // add logic to handle the cell click such as updating game state or sending a move to the server
 
-  if (PLAYER_ACTION == "moving_ship") MoveShip(cell, row, col, gridId, onUpdate);
+  if (PLAYER_ACTION == "moving_ship") MoveShip(cell);
       
-  if (PLAYER_ACTION == "attacking") Attack(cell, row, col, gridId, onUpdate);
-
-  if (PLAYER_ACTION == "placing_ship") PlaceShip(cell, row, col, gridId, onUpdate);
-  
+  if (PLAYER_ACTION == "attacking") Attack(cell);  
 }
 
-//#endregion
-
-//#region map creation
-function CreateBlankMap(rows = 10, cols = 10) {
-  let map = [];
+function CreateBlankGridDict(gridId) {
+  let rows = 10; 
+  let cols = 10;
+  let gridDict = [];
 
   for (let x = 0; x < rows; x++) {
-    map[x] = [];
     for (let y = 0; y < cols; y++) {
-      map[x][y] = "";
+      gridDict[`${gridId}-${x}-${y}`] = "";
     }
   }
-
-  return map;
+  return gridDict;
 }
 
-function CreateAllMaps() {
-  P1_SHIP_MAP = CreateBlankMap();
-  P1_ATTACK_MAP = CreateBlankMap();
-  P2_SHIP_MAP = CreateBlankMap();
-  P2_ATTACK_MAP = CreateBlankMap();
-}
-//#endregion
-
-//#region Saving and Loading map data
-function SaveMapData(cord) {
-  // possibly defunct after changing to backend storage
-  // cords is in the format of "a-0-0" or "b-0-0"
-  // cords[0] is either "a" or "b" to determine which grid it is
-  // cords[1] is the row number and cords[2] is the column number
-  // cordstate is the value of the gamecellvalue of the cell which can be "ship" "hit" "miss" or empty
-  let cordState = document.getElementById(cord).gamecellvalue; 
-
-  // if the cordstate is "ship" then we need to save it to the appropriate ship list based on the player turn
-  if (cordState == "ship") {
-    if (cord[0] == "a" ) {
-      if (PLAYER_TURN == 1) {
-        P1_SHIP_MAP[cord] = cordState;
-      }
-      else {
-        P2_SHIP_MAP[cord] = cordState;
-      }
-      // the game should only try to save cordstate "ship" if the cord is in grid "a" (ship grid)
-    } else throw new Error("attempted to save ship data to grid b or invalid grid");
-  }
-
-  // if the cordstate is "hit" or "miss" then we need to save it to the appropriate ship list and attack list based on the player turn
-  else {
-    if (cord[0] == "a" ) { // grid "a" shows the current player's ships
-      if (PLAYER_TURN == 1) { // if player 1 is attacking when this function is called
-        P2_SHIP_MAP[cord] = cordState; // update the ship list of the opponent player with the new cordstate (hit or miss)
-        P1_ATTACK_MAP[cord] = cordState; // update the attack list of the current player with the new cordstate (hit or miss)
-      }
-      else { // if p2 is attacking then do the update the corresponding lists for player 1 and player 2
-        P1_SHIP_MAP[cord] = cordState;
-        P2_ATTACK_MAP[cord] = cordState;
-      }
-    }
-  }
-}
-
-function SaveAllMapData() {
-  // possibly defunct after changing to backend storage
-  for (let i = 0; i < 10; i++) { // for each row in grid "a" and "b"
-    for (let j = 0; j < 10; j++) { // for each column in each row
-      // if cell is not empty save the data to the appropriate list
-      if (document.getElementById(`a-${i}-${j}`).gamecellvalue != "")
-        SaveMapData(`a-${i}-${j}`);
-
-      // if cell is not empty save the data to the appropriate list
-      if (document.getElementById(`b-${i}-${j}`).gamecellvalue != "")
-        SaveMapData(`b-${i}-${j}`);
-    }
-  }
-}
-
-function LoadAllMapData(setGridA, setGridB) {
+function LoadMapData(setGridA, setGridB) {
   // build two new grids (10x10) and populate them from the backend lists
   const rows = 10;
   const cols = 10;
-  const newA = [];
-  const newB = [];
-  for (let i = 0; i < rows; i++) {
-    newA[i] = [];
-    newB[i] = [];
-    for (let j = 0; j < cols; j++) {
-      const cordA = `a-${i}-${j}`;
-      const cordB = `b-${i}-${j}`;
+  const newGridA = [];
+  const newGridB = [];
+  for (let x = 0; x < rows; x++) {
+    newGridA[x] = [];
+    newGridB[x] = [];
+    for (let y = 0; y < cols; y++) {
+      const cordA = `a-${x}-${y}`;
+      const cordB = `b-${x}-${y}`;
       if (PLAYER_TURN === 1) {
-        newA[i][j] = P1_SHIP_MAP[cordA] || "";
-        newB[i][j] = P1_ATTACK_MAP[cordB] || "";
+        newGridA[x][y] = P1_SHIP_DATA[cordA] || "";
+        newGridB[x][y] = P1_ATTACK_DATA[cordB] || "";
       } else {
-        newA[i][j] = P2_SHIP_MAP[cordA] || "";
-        newB[i][j] = P2_ATTACK_MAP[cordB] || "";
+        newGridA[x][y] = P2_SHIP_DATA[cordA] || "";
+        newGridB[x][y] = P2_ATTACK_DATA[cordB] || "";
       }
     }
   }
 
-  if (typeof setGridA === 'function') setGridA(newA);
-  if (typeof setGridB === 'function') setGridB(newB);
+  if (typeof setGridA === 'function') setGridA(newGridA);
+  if (typeof setGridB === 'function') setGridB(newGridB);
 
   // return the built grids in case caller wants them synchronously
-  return { gridA: newA, gridB: newB };
+  return { gridA: newGridA, gridB: newGridB };
 }
-//#endregion
 
-//#region player actionhandlers
-function PlaceShip(cell, row, col, gridId, onUpdate) {
-  const cord = cell.id;
-  console.log("place ship not implemented"); // handle placing ship logic
+function IdentifyShipFromCell(cellId) {
+  const shipList = (PLAYER_TURN === 1) ? P1_SHIP_DATA : P2_SHIP_DATA;
   
-  SaveMapData(cord);
+  for (const shipName in shipList) {
+    if (shipList[shipName].includes(cellId)) {
+      return shipList[shipName];// return the list of coordinates for this ship
+    }
+  }
+  
+  return null; // no ship found at this cell
 }
 
-function MoveShip(cell, row, col, gridId, onUpdate) {
-  const cord = cell.id;
+function SaveShipToList(shipCells){
+  const shipList = (PLAYER_TURN === 1) ? P1_SHIP_DATA : P2_SHIP_DATA;
+  const shipName = `ship${shipList.length + 1}`; // simple way to generate a new ship name
+  shipList[shipName] = shipCells; // save the ship's coordinates to the list
+}
+
+function PlaceShip(cellId) {
+}
+
+function MoveShip(cellId) {
   console.log("move ship not implemented"); // handle moving ship logic
 
-  SavemapData(cord);
 }
 
-function Attack(cell, row, col, gridId, onUpdate) {
-  const cord = cell.id;
+function Attack(cellId) {
   console.log("attacking not implemented"); // handle attacking logic
 
-  SaveMapData(cord);
-}
-//#endregion
-
-//#region game functions
-function InitGame() {
-  CreateAllMaps();
-  LoadAllMapData();
 }
 
 function EndTurn() {
@@ -273,7 +340,7 @@ function EndTurn() {
   PLAYER_TURN = (PLAYER_TURN === 1) ? 2 : 1;
 
   // load the game state for the new player
-  LoadAllMapData();
+  LoadMapData();
 }
 
 function SetBackgroundOfCell(cellValue){
@@ -282,13 +349,22 @@ function SetBackgroundOfCell(cellValue){
   else if (cellValue == "miss") return "white";
   else return "transparent";
 }
-//#endregion
 
-//#region test functions
+function ChooseGrid(){
+  if (PLAYER_ACTION == "placing_ship") {
+    return (PLAYER_TURN === 1) ? P1_SHIP_DATA : P2_SHIP_DATA;
+  }
+  else if (PLAYER_ACTION == "attacking") {
+    return (PLAYER_TURN === 1) ? P1_ATTACK_DATA : P2_ATTACK_DATA;
+  }
+  else if (PLAYER_ACTION == "moving_ship") {
+    return (PLAYER_TURN === 1) ? P1_SHIP_DATA : P2_SHIP_DATA;
+  }
+}
 
-function changeRandomCells(setGridA, setGridB) {
+function TESTchangeRandomCells(setGridA, setGridB) {
   let randomGrid = Math.random() < 0.5 ? "a" : "b";
-  let random100 = Math.floor(Math.random() * 100);
+  let random100 = Math.floor(Math.random() * 30);
   let random3 = Math.floor(Math.random() * 3);
   
   let randomValue = "";
@@ -301,40 +377,39 @@ function changeRandomCells(setGridA, setGridB) {
     let randomCol = Math.floor(Math.random() * 10);
     let cellId = `${randomGrid}-${randomRow}-${randomCol}`;
     if (randomGrid == "a") {
-      P1_SHIP_MAP[cellId] = randomValue;
-      P2_ATTACK_MAP[cellId] = randomValue;
+      P1_SHIP_DATA[cellId] = randomValue;
+      P2_ATTACK_DATA[cellId] = randomValue;
     }
     else if (randomGrid == "b") {
-      P1_ATTACK_MAP[cellId] = randomValue;
-      P2_SHIP_MAP[cellId] = randomValue;
+      P1_ATTACK_DATA[cellId] = randomValue;
+      P2_SHIP_DATA[cellId] = randomValue;
     }
     console.log(`changed cell ${cellId} to value ${randomValue}`);
   }
   console.log(`changed ${random100} cells in grid ${randomGrid} to value ${randomValue}`);
-  LoadAllMapData(setGridA, setGridB);
+  LoadMapData(setGridA, setGridB);
 }
 
-function ResetMaps(setGridA, setGridB) {
-  P1_ATTACK_MAP = {};
-  P1_SHIP_MAP = {};
-  P2_ATTACK_MAP = {};
-  P2_SHIP_MAP = {};
-  LoadAllMapData(setGridA, setGridB);
+function TESTResetMaps(setGridA, setGridB) {
+  P1_ATTACK_DATA = {};
+  P1_SHIP_DATA = {};
+  P2_ATTACK_DATA = {};
+  P2_SHIP_DATA = {};
+  LoadMapData(setGridA, setGridB);
 }
 
-function AddTestingButtons({ setGridA, setGridB }) {
+function AddTestingButtons() {
+  const { setGridA, setGridB } = useContext(GameContext);
   return (
     <div >
-      <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2" onClick={() => changeRandomCells(setGridA, setGridB)}>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2" onClick={() => TESTchangeRandomCells(setGridA, setGridB)}>
         Change Random Cells
       </button>
-      <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => ResetMaps(setGridA, setGridB)}>
+      <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => TESTResetMaps(setGridA, setGridB)}>
         Reset Maps
       </button>
     </div>
   )
 }
-
-//#endregion
 
 export default App
